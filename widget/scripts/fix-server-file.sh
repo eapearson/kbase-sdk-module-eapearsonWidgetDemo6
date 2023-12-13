d6f2d208539df974566145592f71b745ced0fa28
Erik Pearson <eapearson@lbl.gov>
2023-12-13 12:15:03 -0800

    debugging support

--- widget/scripts/fix-server-file.sh
+++ widget/scripts/fix-server-file.sh
@@ -9,5 +9,5 @@ SERVICE_IMPORT_SNIPPET="${PWD}/widget/scripts/service-server-snippet-0.txt" && \
 SERVICE_PATH_HANDLER_SNIPPET="${PWD}/widget/scripts/service-server-snippet-1.txt" && \
 sed -i.bak-$(date "+%Y-%m-%d-%H-%M-%S") \
     -e "/from configparser import ConfigParser/ r ${SERVICE_IMPORT_SNIPPET}" \
-    -e "/def __call__/ r ${SERVICE_PATH_HANDLER_SNIPPET}" \
+    -e "/status = '500 Internal Server Error'/ r ${SERVICE_PATH_HANDLER_SNIPPET}" \
     ${SOURCE_FILE}
